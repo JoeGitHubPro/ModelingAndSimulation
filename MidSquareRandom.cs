1e359a2e9488716954a442d46b442e82a7423811
JoeGitHubPro <95888292+JoeGitHubPro@users.noreply.github.com>
2023-11-04 17:31:08 +0200

    Initial commit

---
+++ MidSquareRandom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingAndSimulation
+{
+    public class MidSquareRandom
+    {
+        private int seed;
+
+        public MidSquareRandom(int seed)
+        {
+            this.seed = seed;
+        }
+
+        public int Next()
+        {
+            int square = seed * seed;
+            string squareString = square.ToString();
+
+            // Pad the square with leading zeros if necessary
+            while (squareString.Length < 8)
+            {
+                squareString = "0" + squareString;
+            }
+
+            // Extract the middle 4 digits as the next random number
+            string middleDigits = squareString.Substring(2, 4);
+            int nextRandom = int.Parse(middleDigits);
+
+            // Update the seed for the next iteration
+            seed = nextRandom;
+
+            return nextRandom;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ModelingAndSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise;
+            
+            Console.WriteLine("Choose method No...\n1-Mid Square Random\n2-Linear Congruential Random\n3-Multiplicative Congruential Random ");
+
+            choise = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("======================================================================");
+           
+            switch (choise)
+            {
+
+                case 1:
+                    #region MidSquareRandomRegion
+
+                    // Create a new instance of the MidSquareRandom class with a seed value
+                    MidSquareRandom midRandomGenerator = new MidSquareRandom(1234);
+
+                    // Generate and print 10 random numbers
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int randomNumber = midRandomGenerator.Next();
+                        Console.WriteLine(randomNumber);
+                    }
+
+                    #endregion
+                    break;
+                case 2:
+                    #region LinearCongruentialRandomRegion
+
+                    // Create a new instance of the LinearCongruentialRandom class with parameters
+                    LinearCongruentialRandom linearRandomGenerator = new LinearCongruentialRandom(1234, 48271, 0, 2147483647);
+
+                    // Generate and print 10 random numbers
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int randomNumber = linearRandomGenerator.Next();
+                        Console.WriteLine(randomNumber);
+                    }
+
+                    #endregion
+                    break;
+                case 3:
+                    #region MultiplicativeCongruentialRandomRegion
+
+                    // Create a new instance of the MultiplicativeCongruentialRandom class with parameters
+                    MultiplicativeCongruentialRandom multiplicativeRandomGenerator = new MultiplicativeCongruentialRandom(1234, 48271, 2147483647);
+
+                    // Generate and print 10 random numbers
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int randomNumber = multiplicativeRandomGenerator.Next();
+                        Console.WriteLine(randomNumber);
+                    }
+
+                    #endregion
+                    break;
+                default:
+                    Console.WriteLine("there is no choise");
+                    break;
+            }
+
+
+        }
+    }
+}
